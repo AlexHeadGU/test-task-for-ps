fad8da70c0f66d54e3a71571fdace1644ae9c159
Aleksandr <alex-davydov211@mail.ru>
2023-08-01 08:23:09 +0300

    add  files

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div></div>
 </template>
 
+<script>
+  import vCityPart from './components/v-city-part/v-city-part'
+
+  export default {
+  name: 'app',
+  components: {
+    vCityPart
+  },
+  data() {
+    return {}
+  },
+
+  methods: {},
+  mounted() { }
+}
+</script>
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -13,18 +25,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
